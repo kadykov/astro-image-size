0522d471347d7dc18783e7c6b768af5587728fb3
Aleksandr Kadykov <git@kadykov.com>
2024-11-26 09:50:32 +0000

    Use default GitHub Pages address

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://github.kadykov.com/',
+    site: 'https://kadykov.github.io/',
     base: 'astro-image-size',
   })
